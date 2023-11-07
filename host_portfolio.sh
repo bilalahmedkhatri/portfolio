1b8d8c44f615b7a46880d1386a4572a2beaab56f
bilalahmedkhatri <bilal@Aashaaz>
2023-11-08 01:35:52 +0500

    script file updated, (added linux installation packages)

--- host_portfolio.sh
+++ host_portfolio.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
+echo " <<======= linux packages updating ========>> "
+
+apt update && apt-get update && apt upgrade -y
+
+
+echo " {{===== Install python 3.11, python3-pip CURL, GIT =====}} "
+
+apt install -y python3.11 python3-pip curl git postgresql postgresql-contrib && apt update -y
+
+echo " {{===== Starting POSTGRESQL SERVER =====}} "
+
+systemctl restart postgresql.service
 
 echo "installing libraries..."
 
 pip install -r requirements.txt
 
-echo "packages installed."
+echo "required packages installed."
 
 echo "<======= CREATE DATABASE ======>"
 
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+
+python3 manage.py migrate
 
 echo "<(------ CREATE SUPER USER ------)>"
 
-python manage.py createsuperuser
+python3 manage.py createsuperuser
 
 echo "<(------ CREATING STATIC DIRECTORY ------)>"
 
-python manage.py collectstatic
+python3 manage.py collectstatic
 
 sudo apt install -y ufw
 
 echo "<([===== START DJANGO SERVER ON '0.0.0.0:8000' =====])>"
 
-python manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
