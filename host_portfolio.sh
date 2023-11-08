5d74483ccd32f5a7d1db2c0b9c1b7c53dd3f4ba9
bilalahmedkhatri <bilal@Aashaaz>
2023-11-09 02:37:37 +0500

    script file updated "3nd update", (added linux installation packages)

--- host_portfolio.sh
+++ host_portfolio.sh
@@ -3,24 +3,23 @@ echo " <<======= linux packages updating ========>> "
 
 apt update && apt-get update && apt upgrade -y
 
-
 echo " {{===== Install python 3.11, python3-pip CURL, GIT =====}} "
 
 apt install -y python3.11 python3-pip python3-venv curl git postgresql postgresql-contrib && apt update -y
+
+echo "installing libraries..."
+
+pip3 install pipenv
+
+pipenv shell 
 
 echo " {{===== Starting POSTGRESQL SERVER =====}} "
 
 service postgresql start
 
-pip3 install 
-
-echo "installing libraries..."
-
-pip3 install pipenv && pip install -r requirements.txt
-
-pipenv shell
-
 echo "required packages installed."
+
+pip install -r requirements.txt
 
 echo "<======= CREATE DATABASE ======>"
 
