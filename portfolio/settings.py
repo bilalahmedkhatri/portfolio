1b18096adafd1a3b0aa2a2d461dff97f7c61ce02
bilalahmedkhatri <bilalahmedkhatri@outlook.com>
2024-06-02 00:31:23 +0200

    media dir added for hosting

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 from django.core.management.utils import get_random_secret_key
 
 
-RANDOM_KEY = get_random_secret_key()
-print("key :", RANDOM_KEY)
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -15,14 +13,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-SECRET_KEY = RANDOM_KEY
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', "104.248.240.38", "192.168.100.84",
-                 '.vercel.app',  '.now.sh']
+ALLOWED_HOSTS = ['bilalahmed.dev', 'localhost']
+
 # Application definition
 INSTALLED_APPS = [
     'rest_framework',
