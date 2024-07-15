4a6ab351d1c416a647495c62d9d6d60454a32e37
bilalahmedkhatri <bilalahmedkhatri@outlook.com>
2024-07-15 17:10:16 +0200

    db update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -74,8 +74,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
 
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'uihxzefkgh_portfolio',
