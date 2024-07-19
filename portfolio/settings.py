88639188616cae9a375161ea94e0a09476487770
bilalahmedkhatri <bilalahmedkhatri@outlook.com>
2024-07-20 00:00:30 +0200

    bd setting update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,15 +76,15 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
 
-        # 'ENGINE': 'mysql.connector.django',
-        # 'NAME': 'uihxzefkgh_portfolio',
-        # 'USER': 'uihxzefkgh_admin',
-        # 'PASSWORD': 'IhqYAj)+XRRJ',
-        # 'HOST': '120.0.0.1',
-        # 'PORT': 3306,
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'uihxzefkgh_portfolio',
+        'USER': 'uihxzefkgh_admin',
+        'PASSWORD': 'IhqYAj)+XRRJ',
+        'HOST': '120.0.0.1',
+        'PORT': 3306,
 
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
         # 'NAME': os.environ.get("PORTFOLIO_DB_NAME"),
